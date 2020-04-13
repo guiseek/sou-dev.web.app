f333e4b67c315826d4e0b92dfc699f5a0c1254ef
Gui Seek <guiseek@gmail.com>
2020-04-13 18:41:27 -0300

    remove angular plugin

--- scully.sou-dev.config.js
+++ scully.sou-dev.config.js
@@ -1,13 +1,19 @@
+const { RemoveAngular } = require('./dist/remove-angular');
+
+const postRenderers = [RemoveAngular];
+
 exports.config = {
   projectRoot: "./src",
   projectName: "sou-dev",
+  defaultPostRenderers: postRenderers,
   outDir: './dist/static',
   routes: {
     '/blog/:slug': {
       type: 'contentFolder',
       slug: {
         folder: "./blog"
-      }
+      },
+      postRenderers
     },
   }
 };
